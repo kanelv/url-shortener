715cc25098154bcac622217e49a01b9c9e2ae493
Cuong Le <cuonglvrepvn@gmail.com>
2023-07-13 02:40:39 +0900

    minor fix

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -1,12 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { UserService } from './users.service';
+import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [UserService]
     }).compile();
 
     service = module.get<UserService>(UserService);
