bb5702cdc872997768db165ca46ef147a6aff0c3
Cuong Le <cuonglvrepvn@gmail.com>
2023-07-13 02:32:28 +0900

    config circleci +1

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  collectCoverageFrom: ['**/*.service.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ['html'],
+  modulePathIgnorePatterns: [
+    'src/core/database/',
+    'src/main.ts',
+    'src/lambda.ts'
+  ],
+  coverageThreshold: {
+    global: {
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  globalSetup: '../global-setup.js',
+  // Suppress console output during test runs
+  silent: true
+};
