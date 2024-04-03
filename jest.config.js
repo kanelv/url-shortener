47e8f5c6c3c7d0c3cb75d11b40f6c2633d70eb65
Cuong Le <cuonglvrepvn@gmail.com>
2024-04-04 02:48:49 +0900

    fix: disable coverage and coverageThreshold

--- jest.config.js
+++ jest.config.js
@@ -8,20 +8,20 @@ module.exports = {
   collectCoverageFrom: ['**/*.service.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
-  collectCoverage: true,
+  collectCoverage: false,
   coverageReporters: ['html'],
   modulePathIgnorePatterns: [
     'src/core/database/',
     'src/main.ts',
     'src/lambda.ts'
   ],
-  coverageThreshold: {
-    global: {
-      functions: 80,
-      lines: 80,
-      statements: 80
-    }
-  },
+  // coverageThreshold: {
+  //   global: {
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80
+  //   }
+  // },
   globalSetup: '../global-setup.ts',
   // Suppress console output during test runs
   silent: true
